b9cc1ea1e2977aedef1e2b1e68bf3369fc9d49dc
Sam Phippen <samphippen@googlemail.com>
2014-03-23 17:40:42 +0000

    Make a script to add team names to the schedule

---
+++ add_teams.py
@@ -0,0 +1,29 @@
+import random
+from core import *
+
+def teams():
+    return set(json.load(open("teams.json")).keys()) - set(["SRZ"])
+
+if __name__ == "__main__":
+    random.seed("student robotics seed lol")
+
+    shuffled_teams = list(teams())
+    random.shuffle(shuffled_teams)
+
+    schedule = open(schedule_file_name(53)).read()
+    lines = schedule.split("\n")
+    build = ""
+    for line in lines:
+        if len(line) == 0:
+            continue
+
+        if line.strip()[0] == "#":
+            build += line
+        else:
+            parts = line.split("|")
+            parts = [shuffled_teams[int(part)] if part != "x" else part for part in parts]
+            build += "|".join(parts)
+        build += "\n"
+
+    with open("54_with_names.txt", "w") as fp:
+        fp.write(build)
